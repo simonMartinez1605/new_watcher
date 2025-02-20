e50ae1be9bceeb681101298b0eef81eb6b1a0b80
Simon <simonmartinez1605@gmail.com>
2025-02-20 16:55:40 -0500

    Indexing services and Model rewriting

--- models/models.py
+++ models/models.py
@@ -5,75 +5,75 @@ class Model():
     def __init__(self, data_ocr, item): 
         self.data_ocr = data_ocr 
         self.item = item 
-    
-    def search_form(self)-> bool: 
+
+    def find(self): 
         try: 
             for i, word in enumerate(self.data_ocr['text']): 
-                if word == "Applicant":
-                    print(word) 
-                    x,y,w,h = self.data_ocr['left'][i], self.data_ocr['top'][i], self.data_ocr['width'][i], self.data_ocr['height'][i]
 
-                    region_x = x - 60
-                    region_y = y - 75
-                    region_w = 100 
-                    region_h = 37 
+                if word == "APPLICANT":
+                    x,y,w,h = self.data_ocr['left'][i], self.data_ocr['top'][i], self.data_ocr['width'][i], self.data_ocr['height'][i]
+                    region_x = x - 50
+                    region_y = y - 80
+                    region_w = 200 
+                    region_h = 100
 
                     region = self.item.crop((region_x, region_y, region_x + region_w, region_y + region_h))
+                    status = pytesseract.image_to_string(region)
+                    status = status.replace("\n", "")
+                    status = status.replace("/", "")
 
-                    text = pytesseract.image_to_string(region)
-
-                    text = text.replace("\n", "")
-
-                    text = text.replace("/", "")
-
-                    print(f"Document type {text}")
-
-                    if "1485" in text: 
-                        return True 
+                    if "EOIR" in status: 
+                        return True
                     else: 
-                        return False             
+                        return False 
         except Exception as e: 
             print(f"Error in search model: {e}")
-
-    def search_I_485(self)-> dict:
+            return False 
+        
+    def search(self): 
         try: 
-    
             for i, word in enumerate(self.data_ocr['text']): 
-                if word == "Applicant":
-                    print(word) 
+                if word == "APPLICANT":
                     x,y,w,h = self.data_ocr['left'][i], self.data_ocr['top'][i], self.data_ocr['width'][i], self.data_ocr['height'][i]
 
-                    region_x = x - 15
-                    region_y = y + 7
-                    region_w = 470 
-                    region_h = 50 
+                    region_x = x - 20
+                    region_y = y + 15
+                    region_w = 800 
+                    region_h = 45 
 
                     region = self.item.crop((region_x, region_y, region_x + region_w, region_y + region_h))
 
                     name = pytesseract.image_to_string(region)
 
                     name = name.replace("\n", "")
+
                     name = name.replace("/", "")
 
-                    region_x = x - 50
-                    region_y = y + 50
-                    region_w = 470 
-                    region_h = 50 
+                    print(f"Document name {name}") 
 
-                    alien_number_region = self.item.crop((region_x, region_y, region_x + region_w, region_y + region_h))
+                    region_x = x + 145
+                    region_y = y - 1 
+                    region_w = 250 
+                    region_h = 35 
 
-                    alien_number = pytesseract.image_to_string(alien_number_region)
+                    region = self.item.crop((region_x, region_y, region_x + region_w, region_y + region_h))
 
-                    alien_number_pattern = r"A(\d\s?){8,9}"
+                    alien_number = pytesseract.image_to_string(region)
 
-                    alien_number = re.search(alien_number_pattern, alien_number) 
+                    alien_number = alien_number.replace("\n", "")
 
-                    if alien_number: 
-                        alien_number = alien_number.group().replace("\n", "").replace(" ", "-") 
+                    alien_number = alien_number.replace("/", "")
+
+                    alien_number = alien_number.replace(" ", "")
+
+                    print(f"Alien number {alien_number}") 
 
                     return {
-                            'name':name, 
-                            'alien_number':alien_number
-                        } 
-        except Exception as e:
-            print(f"Error in search models {e}")
+                        'name':name, 
+                        'alien_number':alien_number 
+                    } 
+
+                    
+        except Exception as e: 
+            print(f"Error in search model: {e}")
+            return False
--- services/index.py
+++ services/index.py
@@ -12,53 +12,40 @@ load_dotenv()
 save_pdf = os.getenv('SAVE_PDF')
 
 save_pdf = save_pdf.replace("\\", "/") 
-#Funcion para poder obtener el nombre y numero de Alien 
-def indexing(pdf):
+
+def indexing(pdf): 
     pages = convert_from_path(pdf)
-    print(f"Review documents")
+
+    pdf_save = PdfWriter() 
 
     try: 
-        for i in range(0, len(pages), 2):
+        for i in range(0, len(pages)): 
+            
             page1 = pages[i]
-            page2 = pages[i + 1] if i + 1 < len(pages) else None
+            data_ocr_page = pytesseract.image_to_data(page1, output_type=pytesseract.Output.DICT) 
 
-            data_ocr_page1 = pytesseract.image_to_data(page1, output_type=pytesseract.Output.DICT)
-            doc_type = Model(data_ocr_page1, page1).search_form() 
+            doc_type = Model(data_ocr_page, page1).find() 
 
-            if doc_type == True:
-                result = Model(data_ocr_page1, page1).search_I_485() 
-                print(f"Page {i+1}: Indexed")
+            
+            page_path = f"{save_pdf}{doc_type}.pdf"  
+            page1.save(page_path, "PDF") 
+            pdf_save.append(page_path) 
 
-            # Crea un nuevo PdfWriter para cada par de páginas
-            pdf_writer = PdfWriter()
+            if doc_type == True: 
+                result = Model(data_ocr_page, page1).search()
+                output_pdf_path = f"{save_pdf}{result['name']}.pdf"
+                with open(output_pdf_path, "wb") as output_pdf:
+                    pdf_save.write(output_pdf)  
+                print(f"Combined PDF saved as {output_pdf_path}") 
 
-            # Guarda la primera página en el PDF
-            page1_path = f"{save_pdf}{result['name']}.pdf" 
-            page1.save(page1_path, "PDF")
-            pdf_writer.append(page1_path)
+                ocr(output_pdf_path)
 
-            if page2:
-                # Guarda la segunda página en el PDF
-                page2_path = f"{save_pdf}{result['name']}.pdf" 
-                page2.save(page2_path, "PDF")
-                pdf_writer.append(page2_path)
-                print(f"Page {i+2}: Saved")
-    
-            # Guarda el PDF combinado para este par de páginas
-            output_pdf_path = f"{save_pdf}{result['name']}.pdf"
-            with open(output_pdf_path, "wb") as output_pdf:
-                pdf_writer.write(output_pdf)
+                sharepoint(output_pdf_path, f"{result['name']}.pdf", result['alien_number']) 
 
-            ocr(output_pdf_path) 
+                pdf_save = PdfWriter() 
 
-            sharepoint(output_pdf_path ,f"{result['name']}.pdf", result['alien_number']) 
-
-            print(f"Combined PDF saved as {output_pdf_path}")
     except Exception as e: 
         print(f"Error in indexing module : {e}")
 
-
 if __name__ == "__main__":
-    if not os.path.exists(save_pdf):
-        os.makedirs(save_pdf)
-    indexing("c:/Users/SimonMartinez/Documents/Simon/View Folder/OCR/Done/simon.pdf")
+    indexing(save_pdf)   
